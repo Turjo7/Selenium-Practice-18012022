692aaf93e070866aaa15aec3856a20a13404e849
Nasir Uddin Ahmed <43819252+Turjo7@users.noreply.github.com>
2022-01-19 13:00:34 +0600

    Iframe Handling
    Iframe Handling

---
+++ Iframe4.java
@@ -0,0 +1,25 @@
+package com.sel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Iframe4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
+		driver.get("https://demoqa.com/frames");
+		
+		WebElement iframeElement = driver.findElement(By.id("IF1"));
+		driver.switchTo().frame(iframeElement);
+
+	}
+
+}
